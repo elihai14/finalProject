a96a433767580455881800f09114d3b7816a8c9a
DanielElimelech952004 <95daniel2004@gmail.com>
2026-04-15 16:20:13 +0300

    saving local changes before pull

--- BE/routes/users.js
+++ BE/routes/users.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const session = require("express-session");
-const { json } = require("body-parser");
+
 //routes/user.js
 const dbSingleton = require('../dbSingleton');
 
