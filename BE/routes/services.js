f7462d06e0614a0e2c0c474dc86d910a626553f2
DanielElimelech952004 <95daniel2004@gmail.com>
2026-04-28 15:31:06 +0300

    add update-service , admin/add-service and barber/add-service paths in services.js

--- BE/routes/services.js
+++ BE/routes/services.js
@@ -17,37 +17,85 @@ router.post("/", (req, res) => {
       return res.status(500).json({ message: "Internal Server Error" });
     }
     if (results.length === 0) {
-      return res.status(400).json({ message: "There is no any service for this mail" });
+      return res
+        .status(400)
+        .json({ message: "There is no any service for this mail" });
     }
 
     return res.status(200).json(results);
   });
 });
 
-router.post("/add-service" , (req,res) => {
-    // 1. בדיקה שהמשתמש מחובר
-    if (!req.session || !req.session.user) { // בדיקה אם משתמש לא מחובר 
-        return res.status(401).json({ message: 'User not logged in' });
+router.post("/barber/add-service", (req, res) => {
+    // 1. אבטחה: בדיקה שהמשתמש מחובר
+    if (!req.session || !req.session.user) {
+      return res.status(401).json({ message: "User not logged in" });
     }
 
-    const { serviceName , duration } = req.body;
-    const query =
-      "INSERT INTO services(service_name , duration) VALUES (? , ?)";
+  const { serviceName, price, duration } = req.body;
+  const barberMail = req.session.user.mail_address;
 
-    db.query(query , [serviceName , duration] , (err , results) =>{
-        if(err) {
-            return res.status(500).json({ message: "Internal Server Error" });
-        }
-        return res.status(200).json({ message: "השירות נוסף בהצלחה" });
-    })
-})
+  const checkBarberQuery =
+    "SELECT * FROM barber_services WHERE mail_address = ? AND service_name = ?";
+
+  db.query(checkBarberQuery, [barberMail, serviceName], (err, results) => {
+    if (err) return res.status(500).json({ message: "Internal Error" });
+
+    if (results.length > 0) {
+      return res.status(400).json({ message: "השירות כבר קיים בתפריט שלך" });
+    }
+
+  const insertBarberServices ="INSERT INTO barber_services (mail_address, service_name, price,  duration) VALUES (?, ?, ?, ?)";
+
+    db.query(insertBarberServices,[barberMail, serviceName, price, duration],(err) => {
+      if (err)
+        return res.status(500).json({ message: "Error linking service" });
+
+        return res.status(200).json({ message: "השירות נוסף בהצלחה למספרה שלך" });
+      },
+    );
+  });
+});
+
+
+router.post("/admin/add-service", (req, res) => {
+  // 1. אבטחה: בדיקה שהמשתמש מחובר
+  if (!req.session || !req.session.user) {
+    return res.status(401).json({ message: "User not logged in" });
+  }
+  const { serviceName } = req.body;
+
+  const barberMail = req.session.user.mail_address;
+
+  const checkQuery = "SELECT * FROM services WHERE service_name = ?";
+
+  db.query(checkQuery, [serviceName], (err, results) => {
+    if (err) return res.status(500).json({ message: "Internal Error" });
+
+    if (results.length > 0) {
+      return res.status(400).json({ message: "השירות כבר קיים בקטלוג המערכת" });
+    }
+
+    const insertQuery =
+      "INSERT INTO services (service_name, status_service) VALUES (?, 1)";
+
+    db.query(insertQuery, [serviceName], (err) => {
+      if (err)
+        return res
+          .status(500)
+          .json({ message: "Error adding to global services" });
+
+      return res.status(200).json({ message: "השירות נוסף למערכת בהצלחה" });
+    });
+  });
+});
 
 router.post("/remove-service", (req, res) => {
-  // 1. בדיקה שהמשתמש מחובר
-//   if (!req.session || !req.session.user) {
-//     // בדיקה אם משתמש לא מחובר
-//     return res.status(401).json({ message: "User not logged in" });
-//   }
+  // בדיקה שהמשתמש מחובר
+  if (!req.session || !req.session.user) {
+    // בדיקה אם משתמש לא מחובר
+    return res.status(401).json({ message: "User not logged in" });
+  }
 
   const { serviceName } = req.body;
   const query = "UPDATE services SET status_service = 0 WHERE service_name = ?";
@@ -64,19 +112,44 @@ router.post("/remove-service", (req, res) => {
   });
 });
 
-router.post("/price" , (req,res) =>{
-    const {barberMail , serviceName} = req.body;
-    const query =
-      "SELECT price from appointments WHERE barber_mail_address = ? AND service_name = ?";
-    db.query(query , [barberMail , serviceName] , (err,results) => {
-        if(err)
-            return res.status(500).json({ message: "Internal Server Error" });
-        if (results.length === 0) {
-          return res.status(400).json({ message: "Price not found for this service and barber"});
-        }
+router.put("/update-service" , (req, res) => {
+  // בדיקה שהמשתמש מחובר
+  if (!req.session || !req.session.user) {
+  // בדיקה אם משתמש לא מחובר
+    return res.status(401).json({ message: "User not logged in" });
+  }
+  const barberMail = req.session.user.mail_address; 
 
-        return res.status(200).json(results[0]);
-    })
-})
+  const { serviceName, newPrice, newDuration } = req.body;
+  const query =
+    "UPDATE barber_services SET price = ? , duration = ? WHERE service_name = ?                 AND mail_address = ?";
+
+  db.query(query, [newPrice, newDuration,serviceName, barberMail] , (err,results) =>{
+    if (err) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: `השירות ${serviceName} לא חלק מהשירותים שלך` });
+    }
+
+    return res.status(200).json({ message: "השירות עודכן בהצלחה" });
+  });
+});
+
+router.post("/price", (req, res) => {
+  const { barberMail, serviceName } = req.body;
+  const query =
+    "SELECT price from appointments WHERE barber_mail_address = ? AND service_name = ?";
+  db.query(query, [barberMail, serviceName], (err, results) => {
+    if (err) return res.status(500).json({ message: "Internal Server Error" });
+    if (results.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "Price not found for this service and barber" });
+    }
+
+    return res.status(200).json(results[0]);
+  });
+});
 
 module.exports = router;
