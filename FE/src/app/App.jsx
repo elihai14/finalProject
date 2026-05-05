9a978e14b069e4aeb550bcf13fae7a585167a8e0
elihai14 <elihaiafuta@gmail.com>
2026-05-05 14:13:42 +0300

    saving my local changes before pull

--- FE/src/app/App.jsx
+++ FE/src/app/App.jsx
@@ -5,8 +5,8 @@ import classes from "./app.module.css";
  * Main SIte component
  * @returns
  */
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import RegisterForm from "../components/registerForm/RegisterForm";
 import NewAppForm from "../components/newAppForm/NewAppForm";
 import AppList from "../components/appList/AppList";
 import LoginForm from "../components/loginForm/LoginForm";
@@ -19,11 +19,15 @@ function App() {
 
       <nav>
         {/* משתמשים ב-Link במקום ב-<a> כדי למנוע ריענון של הדף */}
-        <Link to="/">התחברות</Link> |<Link to="/about">הרשמה</Link>
+        <Link to="/login">התחברות</Link> |
+        <Link to="/users/register">הרשמה</Link>
       </nav>
 
       <Routes>
         {/* כאן מגדירים איזה נתיב מציג איזו קומפוננטה */}
+        <Route path="/users/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+
         <Route path="/" element={<NewAppForm />} />
         <Route path="/admin-dashboard" element={<AppList />} />
         <Route path="/client-dashboard" element={<AppList />} />
